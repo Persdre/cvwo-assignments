8645fe099855a3f3209fed7564417ebeceb43bed
persdre <persdre@gmail.com>
2020-02-02 12:32:20 +0800

    CRUD functions and Redux

---
+++ todo-app/src/reducers/todosReducer.js
@@ -0,0 +1,34 @@
+import { LOAD_TODOS, ADD_TODO, TOGGLE_TODO, DELETE_TODO } from '/Users/meganwang/todo-api/todo-app/src/actions/actionsTypes.js'
+
+function todosReducer(state = [], action) 
+{
+    switch(action.type) {
+        case LOAD_TODOS:
+            return action.todos;
+
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    title: action.title,
+                    done: false
+                }
+            ];
+
+        case TOGGLE_TODO:
+            return state.map(todo => (todo.id === action.index) 
+                  ? {...todo, done: !todo.done}
+                  : todo
+            );
+
+        case DELETE_TODO:
+            return state.filter(todo => todo.id !== action.index);
+
+        default:
+            return state;    
+    }
+}
+
+export default todosReducer
+
